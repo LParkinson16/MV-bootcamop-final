6238f92ccb57423e17c5a3abcd8d2f6f0b28b3d5
shazeen99 <shazeen.amir@bp.com>
2021-04-16 14:28:02 +0100

    project update

--- src/project.js
+++ src/project.js
@@ -1,14 +1,3 @@
-class Project {
-    constructor(name, description) {
-        this.name = name 
-        this.description = description
-        this.tasks = []
-    }
 
-    addTask(taskName) {
-        const task = new Task({title, description, proj_id})
-        this.tasks.push(task)
-    }
-}
 
-module.exports = Project
+const {sequelize, DataTypes, Model} = require('../db');
