3eb0bf5ac90e0b677671d6f85a88916be71bb05a
Sophia Mcgeown <sopmcgeo@WKMZT3A84AC1.global.publicisgroupe.net>
2021-04-20 11:47:02 +0100

    Minor server change

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const path = require("path");
 const Handlebars = require("handlebars");
 const expressHandlebars = require("express-handlebars");
-const {allowInsecurePrototypeAccess} = require("@handlebars/allow-prototype-access");
-const Tasks = require("./src/task");
+const {
+  allowInsecurePrototypeAccess,
+} = require("@handlebars/allow-prototype-access");
+const Task = require("./src/task");
 const Project = require("./src/project");
 const User = require("./src/user");
 const { sequelize } = require("./db");
